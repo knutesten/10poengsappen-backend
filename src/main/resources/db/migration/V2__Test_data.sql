f251ab481e8629b8d35a24f251ffaa67d373c927
Knut Esten Melandsø Nekså <knutn@netcompany.com>
2017-04-28 13:26:04 +0200

    Fixed bug in insert statement for team

--- src/main/resources/db/migration/V2__Test_data.sql
+++ src/main/resources/db/migration/V2__Test_data.sql
@@ -4,7 +4,7 @@ INSERT INTO user (email, name) VALUES ('johanne.linde@gmail.com', 'Johanne Birgi
 INSERT INTO user (email, name) VALUES ('vegard.harper@gmail.com', 'Vegard Harper');
 INSERT INTO user (email, name) VALUES ('geir.ottersen@gmail.com', 'Geir Ottersen Nipe');
 
-INSERT INTO team (name) VALUES ('PRMS', 'Product Recall Management System');
+INSERT INTO team (display_name, name) VALUES ('PRMS', 'Product Recall Management System');
 
 INSERT INTO user_team (user_id, team_id) VALUES (1, 1);
 INSERT INTO user_team (user_id, team_id) VALUES (2, 1);
